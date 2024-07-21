ea17cd0fc06aa9cbb2c8d3b84f71a351e435ff6b
Aghakarim-dev <hemidzade1010@gmail.com>
2024-07-21 22:42:36 +0400

    Fixed remove task problem

--- script.js
+++ script.js
@@ -16,14 +16,15 @@ function addTask (){
         updateTask()
         
 }
+
 function updateTask (){
     taskList.innerHTML = ""
     for (let i = 0; i < task.length; i++){
-        taskList.innerHTML += `<li class="bg-white w-[100%] h-[5rem] flex justify-between items-center border text-[1.5em] text-[#9BA3AF] pl-[1rem]">${task[i]}<i class="fa-solid fa-trash w-[20%] h-full flex justify-center items-center text-red-600 cursor-pointer border-l-2" onclick="removeTask()"></i></li>`
-
+        taskList.innerHTML += `<li class="bg-white w-[100%] h-[5rem] flex justify-between items-center border text-[1.5em] text-[#9BA3AF] pl-[1rem]">${task[i]}<i class="fa-solid fa-trash w-[20%] h-full flex justify-center items-center text-red-600 cursor-pointer border-l-2" onclick="removeTask(${i})"></i></li>`
     }
 }
+
 function removeTask(index){
-    task.splice(index,1)
+    task.splice(index, 1)
     updateTask()
 }
