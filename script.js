79f6eb8f02504392b7f52ee82a6ce94756bc7ccc
Aghakarim-dev <hemidzade1010@gmail.com>
2025-01-06 22:16:35 +0400

    test

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-let inp = document.getElementById('inp');
-let taskList = document.getElementById('taskList');
+let inp = document.getElementById("inp");
+let taskList = document.getElementById("taskList");
 let task = [];
 
-inp.addEventListener('keydown', function(event){
-    if(event.key === 'Enter'){
-        addTask()
-    }
-})
+inp.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
 
-function addTask (){
-        if(inp.value.trim() !== ""){
-            task.unshift(inp.value.trim())
-            inp.value = ''
-        }
-        updateTask()
-        
+function addTask() {
+  if (inp.value.trim() !== "") {
+    task.unshift(inp.value.trim());
+    inp.value = "";
+  }
+  updateTask();
 }
 
-function updateTask (){
-    taskList.innerHTML = ""
-    for (let i = 0; i < task.length; i++){
-        taskList.innerHTML += `<li class="bg-white w-[100%] h-[5rem] flex justify-between items-center border text-[1.5em] text-[#9BA3AF] pl-[1rem]">${task[i]}<i class="fa-solid fa-trash w-[20%] h-full flex justify-center items-center text-red-600 cursor-pointer border-l-2" onclick="removeTask(${i})"></i></li>`
-    }
+function updateTask() {
+  taskList.innerHTML = "";
+  for (let i = 0; i < task.length; i++) {
+    taskList.innerHTML += `<li class="bg-white w-[100%] h-[5rem] flex justify-between items-center border text-[1.5em] text-[#9BA3AF] pl-[1rem]">${task[i]}<i class="fa-solid fa-trash w-[20%] h-full flex justify-center items-center text-red-600 cursor-pointer border-l-2" onclick="removeTask(${i})"></i></li>`;
+  }
 }
 
-function removeTask(index){
-    task.splice(index, 1)
-    updateTask()
+function removeTask(index) {
+  task.splice(index, 1);
+  updateTask();
 }
